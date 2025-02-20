131dc7fe36a506217f1a4e63bcaf09e56b10cc51
KillianFvt <killian.fauvette@gmail.com>
2025-02-20 14:07:07 +0100

    add openapi schema in project and as a view

--- mspr_covid_ekym/urls.py
+++ mspr_covid_ekym/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('covid_data.urls')),
+    path(
+        "openapi",
+        get_schema_view(
+            title="MSPR Covid Data",
+            description="A project for an MSPR made by Egor, Killian, Martin, Youssef",
+            version="1.0.0"
+        ),
+        name="openapi-schema",
+    ),
 ]
